efeb0e083b2914f8b74a4393fea151c5927ac8ba
ZizyLiz <muh.aziztau05@gmail.com>
2023-12-12 20:50:20 +0700

    editing dashboard and layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -12,8 +12,7 @@
     <title>Inventory</title>
 
     <!-- Custom fonts for this template-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    {{-- <link href="{!! asset('theme/vendor/fontawesome-free/css/all.min.css') !!}" rel="stylesheet" type="text/css"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -67,10 +66,10 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
+                        <a class="collapse-item" href="{{url('kategori')}}">Kategori</a>
                         <a class="collapse-item" href="{{url('barang')}}">Barang</a>
                         <a class="collapse-item" href="{{url('barangmasuk')}}">Barang Masuk</a>
                         <a class="collapse-item" href="{{url('barangkeluar')}}">Barang Keluar</a>
-                        <a class="collapse-item" href="{{url('kategori')}}">Kategori</a>
                     </div>
                 </div>
             </li>
--- resources/views/v_product/dashboard.blade.php
+++ resources/views/v_product/dashboard.blade.php
@@ -77,7 +77,7 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{$kategori}}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            <i class="fa-solid fa-boxes-stacked fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
